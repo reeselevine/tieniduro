3a7b3b445b827e9219562920a247e492639ba2ea
Reese <reeselevine@yahoo.com>
2014-09-30 00:02:56 -0700

    more work on front page

--- js/custom.js
+++ js/custom.js
@@ -19,3 +19,9 @@ $(document).ready(function(){
       }); // end window scroll
    });  // end section function
 }); // close out script
+
+jQuery('ul.nav li.dropdown').hover(function() {
+  jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn();
+}, function() {
+  jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut();
+});
